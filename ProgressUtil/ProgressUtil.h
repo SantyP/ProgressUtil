a42e26f025ee07a6d773b2cc5e9c63e8e720bdc9
Santosh <Santosh@santoshs-mac.local>
2016-12-27 15:26:37 +0530

    Initial Commit

---
+++ ProgressUtil/ProgressUtil.h
@@ -0,0 +1,19 @@
+//
+//  ProgressUtil.h
+//  ProgressUtil
+//
+//  Created by santosh.vishwakarma on 27/12/16.
+//  Copyright © 2016 LM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProgressUtil.
+FOUNDATION_EXPORT double ProgressUtilVersionNumber;
+
+//! Project version string for ProgressUtil.
+FOUNDATION_EXPORT const unsigned char ProgressUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressUtil/PublicHeader.h>
+
+
